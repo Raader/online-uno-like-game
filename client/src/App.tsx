7f384c54fa6c1ce7aea4845804a62a982b02a5e4
Efe <48628713+Raader@users.noreply.github.com>
2020-10-02 15:17:45 +0300

    basic room create and join

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,82 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import io from "socket.io-client";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, useParams, useHistory
+} from "react-router-dom";
 
 function App() {
   const [socket,setSocket] = useState<SocketIOClient.Socket | null>(null);
+  
   useEffect(() =>{
     const socket = io("http://localhost:5000/");
+    
     setSocket(socket);
   },[])
   return (
+    <Router>
     <div className="App">
-      <h1>hello</h1>
+      <Switch>
+        <Route path="/:roomID">
+          <Game socket={socket}></Game>
+        </Route>
+        <Route path="/">
+          <Home socket={socket}></Home>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
+type SocketProps = {
+  socket:SocketIOClient.Socket | null;
+}
+
+function Home(props:SocketProps) {
+  const history = useHistory();
+  useEffect(() =>{
+    if(!props.socket) return;
+    props.socket.on("createRoom",(id:string) =>{
+      history.push("/" + id);
+    })
+  },[props.socket])
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <button onClick={() => {
+          props.socket?.emit("createRoom")
+      }}>Create Room</button>
+      </div>
+      <div>
+        <input placeholder="room id"></input>
+        <button>Join Room</button>
+      </div>
+    </div>
+  )
+}
+
+
+
+function Game(props:SocketProps) {
+  const params = useParams<{roomID:string}>();
+  const [name,setName] = useState("");
+
+  function handleChange(e: { target: HTMLInputElement; }){
+    setName(e.target.value)
+  }
+  return (
+    <div>
+      <h1>Game</h1>
+      <input placeholder="name" value={name} onChange={handleChange}></input>
+      <button onClick={() =>{
+        console.log(params.roomID);
+        props.socket?.emit("joinRoom",params.roomID,name);
+      }}>Join Room</button>
+    </div>
+  )
+}
 export default App;
--- socket/socket.ts
+++ socket/socket.ts
@@ -1,9 +1,67 @@
 import socketio = require("socket.io");
 import http = require("http");
+import uuid = require("uuid");
+
+interface Player{
+    user:User;
+}
+
+class Room{
+    id:string;
+    players: Array<Player> = [];
+
+    constructor(){
+        this.id = uuid.v4();
+    }
+
+    addPlayer(user:User){
+        if(this.players.length < 4){
+            this.players.push({
+                user
+            });
+        }
+    }
+}
+
+class User{
+    socket:socketio.Socket;
+    name:string;
+    room:Room | undefined;
+
+    constructor(socket:socketio.Socket,name:string = ""){
+        this.socket = socket;
+        this.name = name;
+    }
+}
 
 export function createSocket(server:http.Server){
     const io = socketio(server);
+    const users:Array<User> = [];
+    const rooms:Array<Room> = [];
+
     io.on("connection",(socket) =>{
         console.log("a client has connected");
+        const user = new User(socket);
+        users.push(user);
+        
+        socket.on("createRoom",() =>{
+            const room = new Room();
+            rooms.push(room);
+            socket.emit("createRoom",room.id);
+            console.log("new room" + room.id);
+        });
+
+        socket.on("joinRoom",(id:string,name:string) =>{
+            //check if user is already in a room
+            if(user.room) return;
+            const room = rooms.find((val) => val.id === id);
+            //check if the room exists
+            if(!room) return;
+            //join user to the room
+            user.name = name;
+            room.addPlayer(user);
+            user.room = room
+            console.log(`${name} joined room: ${room.id}`);
+        })
     })
 }
