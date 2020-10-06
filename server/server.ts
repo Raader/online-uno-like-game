6ee518bebea00d0aca048292f7f6933aff1580ca
Efe <48628713+Raader@users.noreply.github.com>
2020-10-06 12:24:24 +0300

    fix

--- server/server.ts
+++ server/server.ts
@@ -16,10 +16,9 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
 
   app.get("*", (req,res) =>{
-      res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+      res.sendFile(path.resolve(__dirname,"../","client","build","index.html"));
   })
 }
-
 //listen to port
 const port = process.env.PORT || 5000
 server.listen(port,() =>{
