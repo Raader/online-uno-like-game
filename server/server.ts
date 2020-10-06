4747d694968b1340cfb0207e1ed85aea4cd7d7b9
Efe <48628713+Raader@users.noreply.github.com>
2020-10-06 09:47:13 +0300

    heroku stuff

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,23 @@
 import express = require("express");
 import http = require("http");
 import {createSocket} from "../socket/socket";
+import path = require("path");
 
 //init app, server and socket
 const app = express();
 const server = http.createServer(app);
 createSocket(server);
+
+//serve client
+if(process.env.NODE_ENV === "production"){
+    
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req,res) =>{
+      res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+  })
+}
 
 //listen to port
 const port = process.env.PORT || 5000
